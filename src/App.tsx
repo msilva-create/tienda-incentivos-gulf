f84881ea1bf0db880c2cc08d18fcb609617209f9
msilva-create <msilva@prolub.com.co>
2026-07-29 16:49:39 -0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -166,7 +166,7 @@ const App: React.FC = () => {
   ];
   const [comprobanteData, setComprobanteData] = useState<{ item: RedeemedItem; user: User; id: string; status: string } | null>(null);
   useEffect(() => {
- const APP_VERSION = '1.0.27';
+ const APP_VERSION = '1.0.28';
     const savedVersion = localStorage.getItem('gulf_version');
     if (savedVersion !== APP_VERSION) {
       localStorage.clear();
